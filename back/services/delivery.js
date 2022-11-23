dec1f995be0292e6fc8ff5b1f1ec6984f1cf806e
GrasshopperBears <starday456@naver.com>
2022-11-23 15:13:55 +0900

    배송 assign API 추가

--- back/services/delivery.js
+++ back/services/delivery.js
@@ -1,4 +1,4 @@
-const { deliveryModel } = require("../models");
+const { deliveryModel, userModel } = require("../models");
 
 const createDelivery = async (req, res) => {
   const { from, to } = req.body;
@@ -13,7 +13,25 @@ const createDelivery = async (req, res) => {
   }
 };
 
-const assignDelivery = (req, res) => {};
+const assignDelivery = async (req, res) => {
+  const { id, deliveryManId } = req.body;
+
+  if (!id || !deliveryManId) return res.sendStatus(400);
+
+  try {
+    const deliveryMan = await userModel.findById(deliveryManId);
+    if (!deliveryMan) return res.sendStatus(404);
+
+    const delivery = await deliveryModel.findById(id);
+    if (!delivery) return res.sendStatus(404);
+
+    delivery.deliveredBy = deliveryManId;
+    await delivery.save();
+    res.sendStatus(200);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
 
 const completeDelivery = async (req, res) => {
   const { id } = req.body;
