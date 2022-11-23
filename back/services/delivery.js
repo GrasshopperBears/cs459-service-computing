a1bf8cf8ad0fff490f43403d947d9b6625b5c6a8
GrasshopperBears <starday456@naver.com>
2022-11-23 15:09:42 +0900

    배송 생성 api 구현

--- back/services/delivery.js
+++ back/services/delivery.js
@@ -1,6 +1,17 @@
 const { deliveryModel } = require("../models");
 
-const createDelivery = (req, res) => {};
+const createDelivery = async (req, res) => {
+  const { from, to } = req.body;
+  if (!from || !to) return res.sendStatus(400);
+
+  try {
+    // TODO: 랜덤으로 realDeliveryId 배정 후 추가
+    await deliveryModel.create({ from, to });
+    res.sendStatus(200);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
 
 const assignDelivery = (req, res) => {};
 
