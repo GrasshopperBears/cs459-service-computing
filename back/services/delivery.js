a6ca4fad6864d42baf8a236d8c37e0916ef084b4
GrasshopperBears <starday456@naver.com>
2022-11-23 15:06:16 +0900

    배송완료 서비스 구현

--- back/services/delivery.js
+++ back/services/delivery.js
@@ -1,5 +1,24 @@
+const { deliveryModel } = require("../models");
+
+const createDelivery = (req, res) => {};
+
 const assignDelivery = (req, res) => {};
 
-const completeDelivery = (req, res) => {};
+const completeDelivery = async (req, res) => {
+  const { id } = req.body;
 
-module.exports = { assignDelivery, completeDelivery };
+  if (!id) return res.sendStatus(400);
+
+  try {
+    const delivery = await deliveryModel.findById(id);
+    if (!delivery) return res.sendStatus(404);
+
+    delivery.status = "complete";
+    await delivery.save();
+    res.sendStatus(200);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+};
+
+module.exports = { createDelivery, assignDelivery, completeDelivery };
