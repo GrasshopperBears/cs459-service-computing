85613a547bea99b022ed8de51a01836d4b2e2770
GrasshopperBears <starday456@naver.com>
2022-11-23 15:09:31 +0900

    delivery 모델 일부 수정

--- back/models/delivery.js
+++ back/models/delivery.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const deliverySchema = Schema({
   from: { type: Schema.Types.ObjectId, ref: "User", required: true },
   to: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  deliveredBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  deliveredBy: { type: Schema.Types.ObjectId, ref: "User" },
   status: { type: String, default: "deliverStart" },
-  realDeliveryId: { type: String, required: true },
+  realDeliveryId: { type: String },
 });
 
 module.exports = deliverySchema;
