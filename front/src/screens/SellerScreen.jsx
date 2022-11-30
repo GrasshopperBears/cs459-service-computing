76567d7e390f6183bdd7e9a9f09735dfd18bbf0a
imYourChoi <jeehun@kaist.ac.kr>
2022-12-01 02:56:27 +0900

    Apply: ParcelList to seller screen

--- front/src/screens/SellerScreen.jsx
+++ front/src/screens/SellerScreen.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import Title from "../components/Title";
+import ParcelList from "../components/ParcelList";
+
+const parcels = [
+  { item: "Hoodie", date: "2022/11/14", status: "Delivery Complete." },
+  {
+    item: "Phone Case",
+    date: "2022/11/20",
+    status: "Departed from 경기 부천.",
+  },
+  { item: "Water", date: "2022/11/21", status: "Arrived at 유성." },
+];
+
 const SellerScreen = () => {
-  return <div>Seller Screen</div>;
+  return (
+    <>
+      <Title title="Check you parcels" />
+      <ParcelList parcels={parcels} />
+    </>
+  );
 };
 
 export default SellerScreen;
