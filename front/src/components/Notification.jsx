f2f09be0fc0dc2616e32caf4e7a4d5ede6f0c4b6
imYourChoi <jeehun@kaist.ac.kr>
2022-12-01 02:49:14 +0900

    Add: notification

---
+++ front/src/components/Notification.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Notification = ({ messages }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "var(--background-gray)",
+        padding: "0 16px",
+        borderRadius: 12,
+      }}
+    >
+      {messages.map((message, index) => (
+        <div
+          style={{
+            padding: "16px 0",
+            fontSize: 14,
+            borderTop: index ? "1px solid var(--border)" : undefined,
+          }}
+        >
+          {message}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Notification;
--- front/src/screens/BuyerScreen.jsx
+++ front/src/screens/BuyerScreen.jsx
@@ -1,28 +1,26 @@
 import React from "react";
+
 import Title from "../components/Title";
+import ParcelItem from "../components/ParcelItem";
+import Notification from "../components/Notification";
+
+const messages = [
+  "'Water' has departed from 전라남도 목포",
+  "'Hoodie' has arrived at 경기도 성남",
+  "'Phone Case' is being delivered via 옥천Hub",
+];
 
 const BuyerScreen = () => {
   return (
     <>
-      <Title title="Check you parcels." />
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          borderRadius: 12,
-          border: "1px solid var(--border)",
-          padding: 14,
-          columnGap: 14,
-          cursor: "pointer",
-        }}
-      >
-        <div style={{ flex: "1", fontSize: 16, fontWeight: 600 }}>Hoodie</div>
-        <div style={{ flex: "1", fontSize: 12, color: "var(--text-gray)" }}>
-          2022/11/14 <br />
-          Departed from 경기도 성남
-        </div>
-      </div>
-      <Title title="Notifications." />
+      <Title title="Check you parcels" />
+      <ParcelItem
+        item="Hoodie"
+        date="2022/11/14"
+        status="Departed from 경기도 성남"
+      />
+      <Title title="Notifications" />
+      <Notification messages={messages} />
     </>
   );
 };
