e5b097a19a27529a98dbf99c57718c5e6ae840a3
imYourChoi <jeehun@kaist.ac.kr>
2022-12-02 03:49:03 +0900

    Rename: function

--- front/src/components/template/Container.jsx
+++ front/src/components/template/Container.jsx
@@ -3,11 +3,11 @@ import Header from "./Header";
 import Menu from "./Menu";
 
 const Container = ({ children }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
   return (
     <div style={{ paddingTop: 119 }}>
-      <Header setIsMenuOpen={setIsMenuOpen} />
-      <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <Header setMenuOpen={setMenuOpen} />
+      <Menu isMenuOpen={isMenuOpen} setMenuOpen={setMenuOpen} />
       <div
         style={{
           padding: "0 20px 24px",
