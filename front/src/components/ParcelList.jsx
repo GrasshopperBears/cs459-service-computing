73003d187aa6846838ed423c2a9a7f4bafb348e0
imYourChoi <jeehun@kaist.ac.kr>
2022-12-01 16:54:19 +0900

    Add: empty parcel list

--- front/src/components/ParcelList.jsx
+++ front/src/components/ParcelList.jsx
@@ -3,28 +3,45 @@ import React from "react";
 const ParcelList = ({ parcels }) => {
   return (
     <div style={{ display: "grid", rowGap: 12 }}>
-      {parcels.map((parcel) => (
+      {!parcels.length ? (
         <div
-          key={parcel.item}
           style={{
             display: "flex",
-            alignItems: "center",
             borderRadius: 12,
+            padding: "14px 16px",
+            fontSize: 16,
+            fontWeight: 500,
+            cursor: "not-allowed",
+            color: "var(--text-gray)",
             border: "1px solid var(--border)",
-            padding: 14,
-            columnGap: 14,
-            cursor: "pointer",
           }}
         >
-          <div style={{ flex: "1", fontSize: 16, fontWeight: 600 }}>
-            {parcel.commodity}
-          </div>
-          <div style={{ flex: "1", fontSize: 12, color: "var(--text-gray)" }}>
-            {parcel.date} <br />
-            {parcel.status}
-          </div>
+          No parcels found
         </div>
-      ))}
+      ) : (
+        parcels.map((parcel) => (
+          <div
+            key={parcel.item}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              borderRadius: 12,
+              border: "1px solid var(--border)",
+              padding: "14px 16px",
+              columnGap: 14,
+              cursor: "pointer",
+            }}
+          >
+            <div style={{ flex: "1", fontSize: 16, fontWeight: 600 }}>
+              {parcel.commodity}
+            </div>
+            <div style={{ flex: "1", fontSize: 12, color: "var(--text-gray)" }}>
+              {parcel.date} <br />
+              {parcel.status}
+            </div>
+          </div>
+        ))
+      )}
     </div>
   );
 };
