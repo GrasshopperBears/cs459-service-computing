f16187c4490b7a6e754fda254eb9c4fce9f5ca1c
imYourChoi <jeehun@kaist.ac.kr>
2022-12-02 17:11:20 +0900

    Add: sensor on/off button

--- front/src/screens/DeliveryScreen.jsx
+++ front/src/screens/DeliveryScreen.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../components/Title";
 import DeliveryList from "../components/DeliveryList";
 import parcelsMock from "../static/parcelsMock";
 
 const DeliveryScreen = () => {
+  const [isSensorOn, setSensorOn] = useState(false);
   return (
     <>
       <Title title="Parcels to Delivery" />
       <DeliveryList parcels={parcelsMock} />
+      <div
+        style={{
+          marginTop: 12,
+          textAlign: "center",
+          cursor: "pointer",
+          color: isSensorOn ? "blue" : "red",
+          fontWeight: 600,
+          fontSize: 14,
+        }}
+        onClick={() => setSensorOn(!isSensorOn)}
+      >
+        Sensor {isSensorOn ? "ON" : "OFF"}
+      </div>
     </>
   );
 };
