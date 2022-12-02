4cb6df368df2216feeb90d8cad9f3acd91b334ad
GrasshopperBears <starday456@naver.com>
2022-12-02 23:19:55 +0900

    사용자 검증 미들웨어 제거

--- back/routes/delivery.js
+++ back/routes/delivery.js
@@ -11,6 +11,6 @@ const validateUser = require("../middlewares/validateUser");
 router.get("/delivery", getDeliveries);
 router.post("/create", createDelivery); // 어드민용
 router.post("/assign", assignDelivery); // 어드민용
-router.post("/complete", validateUser, completeDelivery);
+router.post("/complete", completeDelivery);
 
 module.exports = router;
