c584b71d9bae5a2ed5ad3cfb6f58923587fdc5ba
imYourChoi <jeehun@kaist.ac.kr>
2022-12-08 22:35:46 +0900

    Add: description

--- front/src/screens/CompanyScreen.jsx
+++ front/src/screens/CompanyScreen.jsx
@@ -17,6 +17,16 @@ const CompanyScreen = () => {
     <>
       <Title title="Delivery Requests" />
       <RequestList parcels={parcels} />
+      <div
+        style={{
+          paddingTop: "10px",
+          color: "var(--text-gray)",
+          textAlign: "center",
+          fontSize: 14,
+        }}
+      >
+        도착지 기준으로 분류
+      </div>
     </>
   );
 };
