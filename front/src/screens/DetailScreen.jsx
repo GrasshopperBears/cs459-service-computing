0ea0d2807d31933124f1503298892ade22ddcb94
imYourChoi <jeehun@kaist.ac.kr>
2022-12-11 16:20:36 +0900

    Add: use parcel address for delivery status

--- front/src/screens/DetailScreen.jsx
+++ front/src/screens/DetailScreen.jsx
@@ -6,13 +6,33 @@ import Title from "../components/Title";
 import DetailInfo from "../components/DetailInfo";
 import DetailStatus from "../components/DetailStatus";
 import DetailLocation from "../components/DetailLocation";
+import { hubListAddress } from "../static/locations.js";
 
-const messages = [
-  { location: "강남", status: "잡화처리" },
-  { location: "강남", status: "간선상차" },
-  { location: "대전HUB", status: "간선상차" },
-  { location: "유성", status: "간선하차" },
-  { location: "대전유성신성", status: "배달출발" },
+const messages = (from, to) => [
+  { location: from?.split(" ")[1].slice(0, 2), status: "잡화처리" },
+  { location: from?.split(" ")[1].slice(0, 2), status: "간선상차" },
+  {
+    location: hubListAddress.find((region) => region.name === to?.slice(0, 2))
+      ?.list[0].hubName,
+    status: "간선상차",
+  },
+  { location: to?.split(" ")[1].slice(0, 2), status: "간선하차" },
+  {
+    location: to
+      ?.split(" ")
+      .slice(0, 3)
+      .join("")
+      .replaceAll(/시|군|면|동/g, ""),
+    status: "배달출발",
+  },
+  {
+    location: to
+      ?.split(" ")
+      .slice(0, 3)
+      .join("")
+      .replaceAll(/시|군|면|동/g, ""),
+    status: "배달완료",
+  },
 ];
 
 const DetailScreen = () => {
@@ -35,7 +55,18 @@ const DetailScreen = () => {
     <>
       <Title title="Parcel Details" />
       <DetailInfo parcel={parcel} />
-      <DetailStatus messages={messages} />
+      <DetailStatus
+        messages={
+          parcel?.status === "before"
+            ? []
+            : parcel?.status === "complete"
+            ? messages(parcel?.from, parcel?.to)
+            : messages(parcel?.from, parcel?.to).slice(
+                0,
+                Math.ceil(Math.random() * 5 + 1)
+              )
+        }
+      />
       <DetailLocation parcel={parcel} />
     </>
   );
